1b9d364f50ab9d86b2673e3f20253bce2fa4f67a
MarinaRuzhilo <mru@sitecore.net>
2018-12-11 19:27:22 +0200

    #143286:fix

---
+++ src/Sitecore.Support.143286/StartJob.cs
@@ -0,0 +1,28 @@
+﻿namespace Sitecore.Support.Pipelines.ReplaceItemReferences
+{
+  using Sitecore.Configuration;
+  using Sitecore.Diagnostics;
+  using Sitecore.Jobs;
+  using Sitecore.Pipelines.ReplaceItemReferences;
+  using System;
+
+  public class StartJob
+  {
+    public void Process(ReplaceItemReferencesArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      Assert.IsNotNull(args.SourceItem, "args.SourceItem");
+      Assert.IsNotNull(args.CopyItem, "args.CopyItem");
+      ReferenceReplacementJob job = new ReferenceReplacementJob(args.SourceItem, args.CopyItem, Settings.RelinkClonedSubtree, args.FieldIDs);
+
+      if (args.Async)
+      {
+        job.StartAsync();
+      }
+      else
+      {
+        job.Start();
+      }
+    }
+  }
+}
